e9e4c6773a3c87835efd14a6b0a643275b98b7d2
seventeen <2641315365@qq.com>
2025-08-25 16:52:59 +0800

    third commit

--- tlias/src/test/java/com/uestc/JWTTest.java
+++ tlias/src/test/java/com/uestc/JWTTest.java
@@ -4,6 +4,11 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
+
 public class JWTTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
 
 }
